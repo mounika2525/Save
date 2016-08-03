d7ac84d35db5ebaa31e4ef3a87a24ba6828c6dbf
mounika2525 <mounika123.turlapate@gmail.com>
2016-08-03 12:48:56 +0530

    Rejected commit: Default

--- src/main/java/com/doit/Controller/RecordController.java
+++ src/main/java/com/doit/Controller/RecordController.java
@@ -7,10 +7,7 @@ import com.doit.Service.RecordService;
 import com.doit.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,18 +26,43 @@ public class RecordController {
         this.recordService = recordService;
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.GET)
-//    public List<AudioRecords> ByChId(@PathVariable("audioId") Long audioId,@PathVariable("chapterId") Long chapterId,@PathVariable("length") Long length,@PathVariable("listen_Count") Long listen_Count,@PathVariable("rating")
-//            String rating,@PathVariable("reference") String reference,@PathVariable("topicName") String topicName,@PathVariable("url") String url)
-//    {
-//return recordService.saveByChapterId( audioId,  chapterId,  length,  listen_Count,  rating,  reference,  topicName,  url);
-//    }
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+      //    public List<AudioRecords> ByChId(@PathVariable("audioId") Long audioId,@PathVariable("chapterId") Long chapterId,@PathVariable("length") Long length,@PathVariable("listen_Count") Long listen_Count,@PathVariable("rating")
+      //            String rating,@PathVariable("reference") String reference,@PathVariable("topicName") String topicName,@PathVariable("url") String url)
+      //    {
+      //return recordService.saveByChapterId( audioId,  chapterId,  length,  listen_Count,  rating,  reference,  topicName,  url);
+      //    }
 
 
 
 
-    public List<AudioRecords> byChId(){
-        return recordService.saveByChapterId();
+      // public List<AudioRecords> ByChId(){
+      //     return recordService.saveByChapterId();
+      // }
+
+
+
+
+       //   public List<AudioRecords> ByChId(@PathVariable("audioId") Long audioId,@PathVariable("chapterId") Long chapterId)
+       //    {
+       //       return recordService.saveByChapterId(audioId,chapterId);
+       //    }
+
+
+
+
+
+//    public void saveAudios(@RequestParam("topicname") String topicname){
+//        recordService.insert(topicname);
+//}
+
+
+
+
+public void saveAudios(@RequestParam("chapterid") Long chapterid,@RequestParam("length") Long length,@RequestParam("listen_count") Long listen_count,@RequestParam("rating") String rating,
+                       @RequestParam("reference") String reference,@RequestParam("topicname") String topicname,@RequestParam("url") String url)
+    {
+        recordService.insert(chapterid,  length,  listen_count,  rating,  reference,  topicname,  url);
     }
 
 }
