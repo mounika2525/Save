c5b0c28832b8b3da8cb6d7aad96c56e7f3a32f31
mounika2525 <mounika123.turlapate@gmail.com>
2016-08-03 12:50:16 +0530

    Rejected commit: Default

--- src/main/java/com/doit/Service/RecordServiceImpl.java
+++ src/main/java/com/doit/Service/RecordServiceImpl.java
@@ -21,27 +21,50 @@ public class RecordServiceImpl implements RecordService
         this.recordedRepository=recordedRepository;
     }
 
+
+
+    //    @Override
+    //    public List<AudioRecords> saveByChapterId(Long audioId, Long chapterId, Long length, Long listen_Count, Long rating, Long reference, Long topicName, Long url) {
+    //
+    //        return recordedRepository.saveByChapterId(audioId,  chapterId,  length,  listen_Count,  rating,  reference,  topicName,  url);
+    //    }
+    //
+    //    @Override
+    //    public List<AudioRecords> saveByChapterId(Long chapterId) {
+    //        return null;
+    //    }
+
+
+
+
+    //   @Override
+    //    public List<AudioRecords> saveByChapterId() {
+    //        return recordedRepository.saveByChapterId();
+    //    }
+
+
+
+
+     // @Override
+     // public List<AudioRecords> saveByChapterId(Long audioId, Long chapterID) {
+     //     return recordedRepository.saveByChapterId(audioId,chapterID);
+     //  }
+
+
+
+
 //    @Override
-//    public List<AudioRecords> saveByChapterId(Long audioId, Long chapterId, Long length, Long listen_Count, Long rating, Long reference, Long topicName, Long url) {
-//
-//        return recordedRepository.saveByChapterId(audioId,  chapterId,  length,  listen_Count,  rating,  reference,  topicName,  url);
-//    }
-//
-//    @Override
-//    public List<AudioRecords> saveByChapterId(Long chapterId) {
-//        return null;
+//    public void insert( String topicname) {
+//        recordedRepository.insert(topicname);
 //    }
 
 
 
 
-   @Override
-    public List<AudioRecords> saveByChapterId() {
-        return recordedRepository.saveByChapterId();
+    public void insert(Long chapterid, Long length, Long listen_count, String rating, String reference, String topicname, String url)
+    {
+        recordedRepository.insert(chapterid,length,listen_count,rating,reference,topicname,url);
     }
-
-
-
 
 }
 
